a2f5cc1d30cbd5b377ac53a277a241fcb1dd47f7
MvP <mvp.paulo@gmail.com>
2023-05-13 18:18:53 -0400

    takin a form

---
+++ src/components/movieBoxArts.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Movie } from "server/api/routers/tmdb";
+import { Stars } from "./stars";
+
+export function MoviesBoxArts(todo: Movie) {
+  return (
+    <Link href={`/movie/${todo.id}`} className="hover:cursor-pointer">
+      <div key={todo.id} className="flex flex-col items-center gap-4">
+        <Image
+          src={"https://image.tmdb.org/t/p/w185/" + todo.poster_path}
+          width={185}
+          height={278}
+          className="rounded-xl shadow-2xl shadow-orange-500  hover:shadow-lg hover:shadow-orange-500 "
+          alt={todo.title ?? todo.original_title}
+        />
+        <Stars rating={todo.vote_average} />
+        <span className="text-sm ">{todo.title ?? todo.original_title}</span>
+      </div>
+    </Link>
+  );
+}
