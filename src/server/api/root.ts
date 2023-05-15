131fb425bf4becdddd42cbd2dfb5c5525c72d6eb
Marcus Vinícius Peixoto Paulo <marcus.paulo@grupoicts.com.br>
2023-05-15 17:13:27 -0400

    remove example tRPC

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "server/api/trpc";
-import { exampleRouter } from "server/api/routers/example";
 import { tmdbRouter } from "./routers/tmdb";
 
 /**
@@ -8,7 +7,6 @@ import { tmdbRouter } from "./routers/tmdb";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   tmdb: tmdbRouter,
 });
 
