19f50c36df182cb9b1b7bedbce71ec371f0c2128
Marcus Vinícius Peixoto Paulo <mvp.paulo@gmail.com>
2024-03-21 17:11:20 -0400

    Update [search].tsx

--- src/pages/search/[search].tsx
+++ src/pages/search/[search].tsx
@@ -43,20 +43,20 @@ export default function SearchPage() {
     </div>
   );
 }
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getSession(context);
-  if (!session) {
-    return {
-      // redirect: {
-      //   destination: "/login",
-      //   permanent: false,
-      // },
-    };
-  }
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const session = await getSession(context);
+//   if (!session) {
+//     return {
+//       // redirect: {
+//       //   destination: "/login",
+//       //   permanent: false,
+//       // },
+//     };
+//   }
 
-  return {
-    props: {
-      user: session.user,
-    },
-  };
-};
+//   return {
+//     props: {
+//       user: session.user,
+//     },
+//   };
+// };
