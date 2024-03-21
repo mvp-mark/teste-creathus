c9f3167fd13f80196952473be10366a10ac9b456
Marcus Vinícius Peixoto Paulo <mvp.paulo@gmail.com>
2024-03-21 17:09:47 -0400

    Update [search].tsx

--- src/pages/search/[search].tsx
+++ src/pages/search/[search].tsx
@@ -47,10 +47,10 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
   if (!session) {
     return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
+      // redirect: {
+      //   destination: "/login",
+      //   permanent: false,
+      // },
     };
   }
 
